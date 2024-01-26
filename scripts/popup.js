d34cd2fa86d4842ed0d9b45076a896eee5ac0d4d
RuhiGott <ruhigottu@gmail.com>
2024-01-25 18:09:41 -0800

    add popup

---
+++ scripts/popup.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function(){
+    setTimeout(function() {
+        // Add blur class to elements other than .popup
+        document.querySelectorAll("body > :not(.popup)").forEach(function(element) {
+            element.classList.add("blur");
+        });
+
+        // Show the popup
+        document.querySelector(".popup").style.display = "flex";
+    }, 0);
+});
+
+document.querySelector(".x-circle").addEventListener("click", function(){
+    // Remove blur class from elements other than .popup
+    document.querySelectorAll("body > :not(.popup)").forEach(function(element) {
+        element.classList.remove("blur");
+    });
+
+    // Hide the popup
+    document.querySelector(".popup").style.display = "none";
+});
