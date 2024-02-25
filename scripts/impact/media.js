5610e5b06e10b8166f927db73e3372a3daa35ff8
RuhiGott <ruhigottu@gmail.com>
2024-02-24 19:59:16 -0800

    add back button

--- scripts/impact/media.js
+++ scripts/impact/media.js
@@ -1,10 +1,12 @@
 function toggleMedia() {
     var reporters = document.querySelector(".reporters");
     var multiple = document.querySelector(".multiple");
+    var back = document.querySelector(".back");
     var toHide = document.querySelectorAll(".multiple-out");
     
       reporters.style.display = "grid";
       multiple.style.display = "block";
+      back.style.display = "block";
 
       toHide.forEach(function(element) {
         element.style.display = "none";
@@ -16,10 +18,12 @@ function toggleMediaBack() {
     console.log("test");
     var reporters = document.querySelector(".reporters");
     var multiple = document.querySelector(".multiple");
+    var back = document.querySelector(".back");
     var toShow = document.querySelectorAll(".multiple-out");
     
       reporters.style.display = "none";
       multiple.style.display = "none";
+      back.style.display = "none";
 
       toShow.forEach(function(element) {
         element.style.display = "block";
